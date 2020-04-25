f3c431016cad50a60e9c62c6afe79c7a8437a4e6
Rezwan Abir <abir0187@aol.com>
2020-04-25 00:59:28 -0400

    add accuracy score

--- classification/iris_classifier.py
+++ classification/iris_classifier.py
@@ -1,11 +1,17 @@
 # Load Algorithm
 from sklearn import tree
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 iris = load_iris()
 
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.5)
+
+
 algorithm = tree.DecisionTreeClassifier()
-algorithm.fit(iris.data, iris.target)
+algorithm.fit(x_train, y_train)
 
+pred = (algorithm.predict(x_test))
 
-print(algorithm.predict([[6.7, 3.1, 5.6, 2.4]]))
+print(accuracy_score(y_test, pred))
