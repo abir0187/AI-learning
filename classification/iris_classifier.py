2aca60a390092efd2764190dd31ff8687ba53ee6
Rezwan Abir <abir0187@aol.com>
2020-04-25 00:44:17 -0400

    new

--- classification/iris_classifier.py
+++ classification/iris_classifier.py
@@ -4,9 +4,8 @@ from sklearn.datasets import load_iris
 
 iris = load_iris()
 
-
 algorithm = tree.DecisionTreeClassifier()
-algorithm.fit(iris.data, iris.label)
+algorithm.fit(iris.data, iris.target)
 
 
 print(algorithm.predict([[6.7, 3.1, 5.6, 2.4]]))
